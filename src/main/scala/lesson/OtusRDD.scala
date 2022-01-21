16a6b345cd01ffb1b79d38ebcc0a33170d8890a1
vadopolski <vadopolski@gmail.com>
2022-01-21 14:18:54 +0300

    Deleted error compile

--- src/main/scala/lesson/OtusRDD.scala
+++ src/main/scala/lesson/OtusRDD.scala
@@ -5,12 +5,12 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object OtusRDD extends App {
-  val spark = SparkSession.builder()
+  val spark: SparkSession = SparkSession.builder()
     .appName("Introduction to RDDs")
-    .config("spark.master", "local")
+    .config("spark.master", "local[2]")
     .getOrCreate()
 
-  val context = spark.sparkContext
+  val context: SparkContext = spark.sparkContext
 
   case class TaxiZone(
                        LocationID: String,
@@ -19,12 +19,14 @@ object OtusRDD extends App {
                        service_zone: String
                      )
 
-  val taxiZoneRDD = context.textFile("src/main/resources/data/taxi_zones.csv")
+  val value = context.textFile("src/main/resources/data/taxi_zones.csv")
     .map(l => l.split(","))
     .filter(t => t(3).toUpperCase() == t(3))
     .map(t => TaxiZone(t(0), t(1), t(2), t(3)))
     .map(tz => (tz.Borough, 1))
     .reduceByKey(_ + _)
+
+  val taxiZoneRDD = value
     .foreach(x => println(s"${x._1} -> ${x._2}"))
 
 
